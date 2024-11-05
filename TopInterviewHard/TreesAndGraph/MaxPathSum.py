87da771e5622e5cf5f46989514f56ab6b7a2fb0a
Vakul Kataria <vakul.18@gmail.com>
2024-11-05 11:03:30 +0530

    test

--- TopInterviewHard/TreesAndGraph/MaxPathSum.py
+++ TopInterviewHard/TreesAndGraph/MaxPathSum.py
@@ -4,6 +4,54 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+
+    -2
+  -1   -3
+3    4
+
+  2
+1   3
+
+2
+
+
+ 1
+2 3
+
+  2
+-1
+
+(2)
+ leftsum = (-1)
+		left = 0
+   		right = 0
+		return -1
+rightsum = 0
+
+
+
+"""
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        
+	def __init__(self):
+		self.maxPath = float('-inf')	
+
+	def maxPathSum(self, root: Optional[TreeNode]) -> int:
+		self.GetMaxPath(root)
+		return self.maxPath
+	
+	def GetMaxPath(self, node):
+		if not node:
+			return 0
+		
+		leftPathSum = self.GetMaxPath(node.left)
+		rightPathSum = self.GetMaxPath(node.right)
+		print(leftPathSum + node.val + rightPathSum)
+		
+		currentPathValue = max(leftPathSum + node.val, rightPathSum + node.val, node.val, leftPathSum + node.val + rightPathSum)
+		if currentPathValue > self.maxPath:
+			self.maxPath = currentPathValue
+
+		return max(leftPathSum + node.val, rightPathSum + node.val, node.val)
+
+	
